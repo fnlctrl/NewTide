73adac2219fc9eb1809facbe6822846621dc1308
fnlctrl <fnlctrl@gmail.com>
2014-11-12 03:32:18 +0800

    fixed comment avatar size bug

--- Web/js/main.js
+++ Web/js/main.js
@@ -227,7 +227,7 @@ $(function(){
 				columnGap: W*0.02,
 				standardiseLineHeight: true,
 				lineHeight: status.LineHeight,
-				showGrid: true,
+				//showGrid: true,
 				columnFragmentMinHeight: 144,
 				pagePadding: W*0.04,
 				noWrapOnTags: ['div','img','blockquote']
