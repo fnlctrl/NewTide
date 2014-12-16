777ff771adbbaaa46b99c4904dfc7526f2ee7625
fnlctrl <fnlctrl@gmail.com>
2014-12-16 21:55:31 +0800

    quick fix

--- Web/js/main.js
+++ Web/js/main.js
@@ -284,295 +284,6 @@ $(function(){
 		})()
 	}
 
-	var book = {
-		timer: null,
-		columnizer: null,
-		renderArea: null,
-		getConfig: function(W) { // W is the proper(calculated) width for bookContainer(the width of 2 pages)
-			var H = $window.height();
-			return {
-				columnCount: status.numColumns,
-				viewportHeight: Math.max($window.height()-100,500),
-				viewportWidth: W/2,
-				columnGap: W*0.02,
-				standardiseLineHeight: true,
-				lineHeight: status.LineHeight,
-				//showGrid: true,
-				columnFragmentMinHeight: 140,
-				pagePadding: W*0.04,
-				noWrapOnTags: ['div','img','blockquote']
-			}
-		},
-		init: function(pageW) {
-			//preprocessing wordpress-generated content for FTColumnflow to render
-			if (!status.needBook) {
-				return
-			}
-			var flowedContent; 
-			var fixedContent;
-			if (!status.isListPage) {
-				var $wpEntryImgs = $wpEntryContent.find('img');
-				$wpEntryImgs.each(function() {
-					var $this = $(this);
-					if ($this.parent('p').length) {
-						$this.unwrap();
-					}
-				})
-				if ($wpEntryContent.length) {
-					if ($('.wp-entry-comments').length) {
-						flowedContent = $wpEntryContent[0].innerHTML + $('.wp-entry-comments').html();
-					} else {
-						flowedContent = $wpEntryContent[0].innerHTML;
-					}
-				} else {
-					flowedContent = '';
-				}
-				if ($wpEntryMeta.length) {
-					function handler() {
-						$wpEntryMeta.children().addClass('col-span-2');
-						fixedContent = $wpEntryMeta[0].innerHTML;
-						render(fixedContent);
-					}
-					if ($wpEntryThumbnail.length) {
-						if ($wpEntryThumbnail[0].complete) {
-							handler();
-						} else {
-							$wpEntryThumbnail.load(handler);
-						}
-					} else {
-						handler();
-					}
-				} else {
-					fixedContent ='';
-					render(fixedContent);
-				}
-			} else {
-				flowedContent = $wpWrapper.html();
-				console.log($wpWrapper.html());
-				fixedContent = '';
-				render(fixedContent);
-			}
-			function render(fixedContent) {
-				var cfg = book.getConfig(pageW*2);
-				book.columnizer = new FTColumnflow('book-pages', 'book-container', cfg);
-				book.columnizer.flow(flowedContent, fixedContent);
-				book.renderArea = $('.cf-render-area');
-				book.enableTurningPages(pageW);
-				book.copyEntryThumbnail();
-				book.setUpEvents();
-			}
-		},
-		reflow: function(cfg) {
-			if (!status.needBook) {
-				return
-			}
-			$bookLoadingShade.css({opacity:1});
-			clearTimeout(book.timer);
-			book.timer = setTimeout(book.performReflow(cfg), 500);
-		},
-		performReflow: function(cfg) {
-			return function() {
-				book.columnizer.reflow(cfg);
-				book.renderArea.bookblock('destroy');
-				book.enableTurningPages(cfg.viewportWidth);
-				book.copyEntryThumbnail();
-				$bookLoadingShade.css({opacity:0});
-			}
-		},
-		copyEntryThumbnail: function() { // a workaround to show title image with zero padding
-			if ($wpEntryThumbnail.length) {
-				var $firstPage = $('.cf-page-1');
-				var $div = $('<div />');
-				var img = new Image();
-				img.src = $wpEntryThumbnail[0].src;
-				img.className = 'wp-fake-thumbnail';
-				var h = $firstPage.find('.wp-entry-thumbnail').height() + 35;
-				if (h > 500) {
-					h = 500; // 500 is the max-width of .wp-entry-thumbnail + 50(top page padding)
-				}
-				$div.addClass('wp-fake-thumbnail-wrapper').height(h).append(img);
-				// add sharing buttons
-				var $shareWrapper = $('<div id="share-wrapper" class="toolbar-wrapper"/>');
-				var href = util.getShareLink();
-				$shareWrapper.append('<a class="toolbar-icon douban" target="_blank" href="'+href.douban+'"></a>')
-							.append('<a class="toolbar-icon weibo" target="_blank" href="'+href.weibo+'"></a>')
-							.append('<a class="toolbar-icon renren" target="_blank" href="'+href.renren+'"></a>')
-							.append('<a class="toolbar-icon weixin"></a>')
-							.append('<div id="toolbar-qrcode-wrapper"><div id="toolbar-qrcode"></div><span>分享到微信</span></div>');
-				$div.append($shareWrapper);
-				// add edit button
-				var $postEditLink = $('.post-edit-link');
-				if ($postEditLink.length) {
-					var $adminToolsWrapper = $('<div id="admin-tools-wrapper" class="toolbar-wrapper"/>');
-					var editLink = $postEditLink[0].href;
-					$adminToolsWrapper.append('<a class="toolbar-icon edit" target="_blank" href="'+editLink+'"></a>');
-					$div.append($adminToolsWrapper);
-				}
-				$firstPage.find('.wp-entry-thumbnail').css({opacity:0,height:h-50});
-                $firstPage.append($div);
-				// copy qrcode
-				setTimeout(function(){
-					$('#toolbar-qrcode').append(status.qrcode);
-				},50)
-			}
-		},
-		enableTurningPages: function(pageW) { // pageW is half the width of book-container
-			// group cf-pages by two and wrap them to a .bb-item
-			var $pages = $('.cf-page:not(.cf-preload)');
-			var len = $pages.length;
-			$pages.each(function(index) {
-				var $this = $(this);
-				if (index % 2 === 0) {
-					$this.css({left:0});
-				} else {
-					$this.css({left:pageW});
-					$pages.slice(index-1,index+1).wrapAll('<div class="bb-item"/>');
-				}
-			});
-			if (len % 2 != 0) {
-				$pages.last().wrap('<div class="bb-item"/>');
-			}
-			var $bbItem = $('.bb-item');
-			status.numPages = $bbItem.length;
-			if (status.numPages === 0) {
-				return
-			}
-			// add fake menu icon
-			$bbItem.each(function(){
-				$(this).append($('<div class="menu-icon-fake" />'));//
-			});
-			// enable page flip
-			book.renderArea.addClass('bb-bookblock');
-			var startPage;
-			if (localStorage.returnedFromNextPage) {
-				if (localStorage.returnedFromNextPage === 'true') {
-					startPage = status.numPages;
-				}
-			} else {
-				startPage = 1;
-			}
-			if (!status.isListPage) {
-				startPage = 1;
-			}
-			book.renderArea.bookblock();
-			book.renderArea.data('bookblock')._init({
-				startPage : startPage,
-				speed : 600,
-				shadows: true,
-				shadowSides	: 0.8,
-				shadowFlip	: 0.4,
-				onBeforeFlip: function(page) {
-					status.currentPage = page+1;
-					$menuIcon.css({'opacity':'0'});
-				},
-				onEndFlip: function(old,page,isLimit) {
-					//status.currentPage = page+1;
-					$menuIcon.css({'opacity':'1'});
-				}
-			});
-			localStorage.removeItem('returnedFromNextPage');
-			book.renderArea.focus();
-		},
-		setUpEvents: function() {
-			$bookContainer.click(function(e) {
-				var offset = $bookContainer.offset();
-				if (e.pageY>50 && e.pageY<90 && e.pageX>offset.left && e.pageX<offset.left+25) { // prevent fireing when clicked on menu icon
-					return;
-				}
-				if (e.pageX < offset.left+65) {
-					book.turn.call(book.renderArea,'left');
-				} else if (e.pageX > offset.left+$bookContainer.width()-65) {
-					book.turn.call(book.renderArea,'right');
-				}
-			});
-			$window.keydown(function(e) {
-				var keyCode = e.keyCode || e.which;
-				switch (keyCode) {
-					case 37: //left
-						book.turn.call(book.renderArea,'left');
-						break;
-					case 39: //right
-						book.turn.call(book.renderArea,'right');
-						break;
-					default:
-						break;
-				}
-			});
-			if (!status.isMobile) {
-				$window.on('mousewheel', function(event) {
-					if (event.deltaY < 0){
-						book.turn.call(book.renderArea,'right');
-					} else {
-						book.turn.call(book.renderArea,'left');
-					}
-				});
-			}
-            if (status.isMobile || /iPad/i.test(navigator.userAgent)) {
-                var hammertime = new Hammer($bookContainer[0]);
-                hammertime.on('swipeleft', function(e) {
-                    book.turn.call(book.renderArea,'right');
-                }).on('swiperight',function(){
-                    book.turn.call(book.renderArea,'left');
-                });
-            }
-			//show hints to turn pages
-			$bookContainer.bind('mousemove',function(e) {
-				var offset = $bookContainer.offset();
-				if (e.pageY>50 && e.pageY<90 && e.pageX>offset.left && e.pageX<offset.left+25) {
-					$bookNavPrev.css({opacity:0});
-					$bookNavNext.css({opacity:0});
-				} else {
-					if (e.pageX < offset.left+450) {
-						$bookNavPrev.css({opacity:1});
-					} else if (e.pageX > offset.left+$bookContainer.width()-450) {
-						$bookNavNext.css({opacity:1});
-					} else {
-						$bookNavPrev.css({opacity:0});
-						$bookNavNext.css({opacity:0});
-					}
-				}
-			}).bind('mouseleave',function() {
-				$bookNavPrev.css({opacity:0});
-			});
-		},
-		turn: function(direction) {
-			if (direction==='left') {
-				this.data('bookblock').prev(); // 'this' is book.renderArea passed in by Function.prototype.call()
-				if (status.currentPage === 1) {
-					if (status.isListPage) {
-						if (/page/i.test(location.href)) {
-							location.href = status.prevPageURL;
-							localStorage.setItem('returnedFromNextPage','true');
-						} else {
-							util.showNotice('这个分类下已经没有更新的文章了~');
-						}
-					} else {
-						if (status.prevPageURL != '') {
-							location.href = status.prevPageURL;
-						} else {
-							util.showNotice('这个分类下已经没有更新的文章了~');
-						}
-					}
-				}
-			}
-			else if (direction==='right') {
-				this.data('bookblock').next();
-				if (status.currentPage === status.numPages) {
-					if (status.isListPage) {
-						if ($wpWrapper.find('.wp-item').length >= 60) { // 60 is 3*4*5 that allow entry list to be separated into full pages
-							location.href = status.nextPageURL;
-						} else {
-							util.showNotice('这个分类下已经没有更多文章了~');
-						}
-					} else {
-						if (status.nextPageURL!=='') {
-							location.href = status.nextPageURL;
-						}
-					}
-				}
-			}
-		}
-	};
 	var side_ctrl = {
 		click_x			: 0,
 		start_x			: 0,
@@ -765,6 +476,296 @@ $(function(){
 		}
 		side_ctrl.initialize();
 	} else {
+		var book = {
+			timer: null,
+			columnizer: null,
+			renderArea: null,
+			getConfig: function(W) { // W is the proper(calculated) width for bookContainer(the width of 2 pages)
+				var H = $window.height();
+				return {
+					columnCount: status.numColumns,
+					viewportHeight: Math.max($window.height()-100,500),
+					viewportWidth: W/2,
+					columnGap: W*0.02,
+					standardiseLineHeight: true,
+					lineHeight: status.LineHeight,
+					//showGrid: true,
+					columnFragmentMinHeight: 140,
+					pagePadding: W*0.04,
+					noWrapOnTags: ['div','img','blockquote']
+				}
+			},
+			init: function(pageW) {
+				//preprocessing wordpress-generated content for FTColumnflow to render
+				if (!status.needBook) {
+					return
+				}
+				var flowedContent;
+				var fixedContent;
+				if (!status.isListPage) {
+					var $wpEntryImgs = $wpEntryContent.find('img');
+					$wpEntryImgs.each(function() {
+						var $this = $(this);
+						if ($this.parent('p').length) {
+							$this.unwrap();
+						}
+					})
+					if ($wpEntryContent.length) {
+						if ($('.wp-entry-comments').length) {
+							flowedContent = $wpEntryContent[0].innerHTML + $('.wp-entry-comments').html();
+						} else {
+							flowedContent = $wpEntryContent[0].innerHTML;
+						}
+					} else {
+						flowedContent = '';
+					}
+					if ($wpEntryMeta.length) {
+						function handler() {
+							$wpEntryMeta.children().addClass('col-span-2');
+							fixedContent = $wpEntryMeta[0].innerHTML;
+							render(fixedContent);
+						}
+						if ($wpEntryThumbnail.length) {
+							if ($wpEntryThumbnail[0].complete) {
+								handler();
+							} else {
+								$wpEntryThumbnail.load(handler);
+							}
+						} else {
+							handler();
+						}
+					} else {
+						fixedContent ='';
+						render(fixedContent);
+					}
+				} else {
+					flowedContent = $wpWrapper.html();
+					console.log($wpWrapper.html());
+					fixedContent = '';
+					render(fixedContent);
+				}
+				function render(fixedContent) {
+					var cfg = book.getConfig(pageW*2);
+					book.columnizer = new FTColumnflow('book-pages', 'book-container', cfg);
+					book.columnizer.flow(flowedContent, fixedContent);
+					book.renderArea = $('.cf-render-area');
+					book.enableTurningPages(pageW);
+					book.copyEntryThumbnail();
+					book.setUpEvents();
+				}
+			},
+			reflow: function(cfg) {
+				if (!status.needBook) {
+					return
+				}
+				$bookLoadingShade.css({opacity:1});
+				clearTimeout(book.timer);
+				book.timer = setTimeout(book.performReflow(cfg), 500);
+			},
+			performReflow: function(cfg) {
+				return function() {
+					book.columnizer.reflow(cfg);
+					book.renderArea.bookblock('destroy');
+					book.enableTurningPages(cfg.viewportWidth);
+					book.copyEntryThumbnail();
+					$bookLoadingShade.css({opacity:0});
+				}
+			},
+			copyEntryThumbnail: function() { // a workaround to show title image with zero padding
+				if ($wpEntryThumbnail.length) {
+					var $firstPage = $('.cf-page-1');
+					var $div = $('<div />');
+					var img = new Image();
+					img.src = $wpEntryThumbnail[0].src;
+					img.className = 'wp-fake-thumbnail';
+					var h = $firstPage.find('.wp-entry-thumbnail').height() + 35;
+					if (h > 500) {
+						h = 500; // 500 is the max-width of .wp-entry-thumbnail + 50(top page padding)
+					}
+					$div.addClass('wp-fake-thumbnail-wrapper').height(h).append(img);
+					// add sharing buttons
+					var $shareWrapper = $('<div id="share-wrapper" class="toolbar-wrapper"/>');
+					var href = util.getShareLink();
+					$shareWrapper.append('<a class="toolbar-icon douban" target="_blank" href="'+href.douban+'"></a>')
+						.append('<a class="toolbar-icon weibo" target="_blank" href="'+href.weibo+'"></a>')
+						.append('<a class="toolbar-icon renren" target="_blank" href="'+href.renren+'"></a>')
+						.append('<a class="toolbar-icon weixin"></a>')
+						.append('<div id="toolbar-qrcode-wrapper"><div id="toolbar-qrcode"></div><span>分享到微信</span></div>');
+					$div.append($shareWrapper);
+					// add edit button
+					var $postEditLink = $('.post-edit-link');
+					if ($postEditLink.length) {
+						var $adminToolsWrapper = $('<div id="admin-tools-wrapper" class="toolbar-wrapper"/>');
+						var editLink = $postEditLink[0].href;
+						$adminToolsWrapper.append('<a class="toolbar-icon edit" target="_blank" href="'+editLink+'"></a>');
+						$div.append($adminToolsWrapper);
+					}
+					$firstPage.find('.wp-entry-thumbnail').css({opacity:0,height:h-50});
+					$firstPage.append($div);
+					// copy qrcode
+					setTimeout(function(){
+						$('#toolbar-qrcode').append(status.qrcode);
+					},50)
+				}
+			},
+			enableTurningPages: function(pageW) { // pageW is half the width of book-container
+				// group cf-pages by two and wrap them to a .bb-item
+				var $pages = $('.cf-page:not(.cf-preload)');
+				var len = $pages.length;
+				$pages.each(function(index) {
+					var $this = $(this);
+					if (index % 2 === 0) {
+						$this.css({left:0});
+					} else {
+						$this.css({left:pageW});
+						$pages.slice(index-1,index+1).wrapAll('<div class="bb-item"/>');
+					}
+				});
+				if (len % 2 != 0) {
+					$pages.last().wrap('<div class="bb-item"/>');
+				}
+				var $bbItem = $('.bb-item');
+				status.numPages = $bbItem.length;
+				if (status.numPages === 0) {
+					return
+				}
+				// add fake menu icon
+				$bbItem.each(function(){
+					$(this).append($('<div class="menu-icon-fake" />'));//
+				});
+				// enable page flip
+				book.renderArea.addClass('bb-bookblock');
+				var startPage;
+				if (localStorage.returnedFromNextPage) {
+					if (localStorage.returnedFromNextPage === 'true') {
+						startPage = status.numPages;
+					}
+				} else {
+					startPage = 1;
+				}
+				if (!status.isListPage) {
+					startPage = 1;
+				}
+				book.renderArea.bookblock();
+				book.renderArea.data('bookblock')._init({
+					startPage : startPage,
+					speed : 600,
+					shadows: true,
+					shadowSides	: 0.8,
+					shadowFlip	: 0.4,
+					onBeforeFlip: function(page) {
+						status.currentPage = page+1;
+						$menuIcon.css({'opacity':'0'});
+					},
+					onEndFlip: function(old,page,isLimit) {
+						//status.currentPage = page+1;
+						$menuIcon.css({'opacity':'1'});
+					}
+				});
+				localStorage.removeItem('returnedFromNextPage');
+				book.renderArea.focus();
+			},
+			setUpEvents: function() {
+				$bookContainer.click(function(e) {
+					var offset = $bookContainer.offset();
+					if (e.pageY>50 && e.pageY<90 && e.pageX>offset.left && e.pageX<offset.left+25) { // prevent fireing when clicked on menu icon
+						return;
+					}
+					if (e.pageX < offset.left+65) {
+						book.turn.call(book.renderArea,'left');
+					} else if (e.pageX > offset.left+$bookContainer.width()-65) {
+						book.turn.call(book.renderArea,'right');
+					}
+				});
+				$window.keydown(function(e) {
+					var keyCode = e.keyCode || e.which;
+					switch (keyCode) {
+						case 37: //left
+							book.turn.call(book.renderArea,'left');
+							break;
+						case 39: //right
+							book.turn.call(book.renderArea,'right');
+							break;
+						default:
+							break;
+					}
+				});
+				if (!status.isMobile) {
+					$window.on('mousewheel', function(event) {
+						if (event.deltaY < 0){
+							book.turn.call(book.renderArea,'right');
+						} else {
+							book.turn.call(book.renderArea,'left');
+						}
+					});
+				}
+				if (status.isMobile || /iPad/i.test(navigator.userAgent)) {
+					var hammertime = new Hammer($bookContainer[0]);
+					hammertime.on('swipeleft', function(e) {
+						book.turn.call(book.renderArea,'right');
+					}).on('swiperight',function(){
+						book.turn.call(book.renderArea,'left');
+					});
+				}
+				//show hints to turn pages
+				$bookContainer.bind('mousemove',function(e) {
+					var offset = $bookContainer.offset();
+					if (e.pageY>50 && e.pageY<90 && e.pageX>offset.left && e.pageX<offset.left+25) {
+						$bookNavPrev.css({opacity:0});
+						$bookNavNext.css({opacity:0});
+					} else {
+						if (e.pageX < offset.left+450) {
+							$bookNavPrev.css({opacity:1});
+						} else if (e.pageX > offset.left+$bookContainer.width()-450) {
+							$bookNavNext.css({opacity:1});
+						} else {
+							$bookNavPrev.css({opacity:0});
+							$bookNavNext.css({opacity:0});
+						}
+					}
+				}).bind('mouseleave',function() {
+					$bookNavPrev.css({opacity:0});
+				});
+			},
+			turn: function(direction) {
+				if (direction==='left') {
+					this.data('bookblock').prev(); // 'this' is book.renderArea passed in by Function.prototype.call()
+					if (status.currentPage === 1) {
+						if (status.isListPage) {
+							if (/page/i.test(location.href)) {
+								location.href = status.prevPageURL;
+								localStorage.setItem('returnedFromNextPage','true');
+							} else {
+								util.showNotice('这个分类下已经没有更新的文章了~');
+							}
+						} else {
+							if (status.prevPageURL != '') {
+								location.href = status.prevPageURL;
+							} else {
+								util.showNotice('这个分类下已经没有更新的文章了~');
+							}
+						}
+					}
+				}
+				else if (direction==='right') {
+					this.data('bookblock').next();
+					if (status.currentPage === status.numPages) {
+						if (status.isListPage) {
+							if ($wpWrapper.find('.wp-item').length >= 60) { // 60 is 3*4*5 that allow entry list to be separated into full pages
+								location.href = status.nextPageURL;
+							} else {
+								util.showNotice('这个分类下已经没有更多文章了~');
+							}
+						} else {
+							if (status.nextPageURL!=='') {
+								location.href = status.nextPageURL;
+							}
+						}
+					}
+				}
+			}
+		};
+
 		if ($wpWrapper.length) {
 			status.needBook = true;
 		}
