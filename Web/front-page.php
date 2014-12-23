1615c71acf7121b4db1c3e51a34247bdc4ea4247
fnlctrl <fnlctrl@gmail.com>
2014-12-24 01:09:48 +0800

    quickfix

--- Web/front-page.php
+++ Web/front-page.php
@@ -71,7 +71,7 @@ Template Name: 主页
 			</div>
 	<?php endif?>
 			<div class='posts-wrapper ease'>
-				<h1>编辑精选 <?php if(!$isMobile): ?>	<a class='more' href="<?php echo home_url().'/editors-picks'?>">更多...</a><?php endif;?></h1>
+				<h1>编辑精选 <?php if(!$isMobile): ?>	<a class='more' href="<?php echo home_url().'/category/editors-picks'?>">更多...</a><?php endif;?></h1>
 				<div class='posts-container'>
 					<?php
 					$args1 = array(
@@ -103,7 +103,7 @@ Template Name: 主页
 						</div>
 						<?php //End Loop  ?>
 					<?php endforeach; wp_reset_postdata();?>
-					<?php if($isMobile): ?>	<a class='more' href="<?php echo home_url().'/editors-picks'?>">更多...</a><?php endif;?>
+					<?php if($isMobile): ?>	<a class='more' href="<?php echo home_url().'/category/editors-picks'?>">更多...</a><?php endif;?>
 				</div>
 				<h1>最新投稿 <?php if(!$isMobile): ?>	<a class='more' href="<?php echo home_url().'/all'?>">更多...</a><?php endif;?></h1>
 				<div class='posts-container'>
