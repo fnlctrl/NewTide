499dbbfadff5c260d8a789a3b81fe54f96f3d437
fnlctrl <fnlctrl@gmail.com>
2015-01-01 02:39:02 +0800

    changed piwik script location

--- Web/sidebar.php
+++ Web/sidebar.php
@@ -115,4 +115,19 @@
 	<?php if (!$isMobile):?>
 	<div id='sidebar-copyright'>© 2001 - 2014 <br/> 浙江大学求是潮</div>
 	<?php endif; ?>
+	<!-- Piwik -->
+	<script type="text/javascript">
+		var _paq = _paq || [];
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//piwik.zjuqsc.com/";
+			_paq.push(['setTrackerUrl', u+'piwik.php']);
+			_paq.push(['setSiteId', 3]);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<noscript><p><img src="//piwik.zjuqsc.com/piwik.php?idsite=3" style="border:0;" alt="" /></p></noscript>
+	<!-- End Piwik Code -->
 </div>
